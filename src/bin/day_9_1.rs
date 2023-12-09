83ac902d66c01d34a2810b1138014c15db5d6b50
Paul Johannes Wagner <hydrofin@gmail.com>
2023-12-09 12:10:56 +0100

    solve day 9

---
+++ src/bin/day_9_1.rs
@@ -0,0 +1,32 @@
+use rayon::prelude::*;
+
+fn main() {
+    let start_time = std::time::Instant::now();
+    let input = include_str!("../../data/day_9.txt");
+
+    let result: i64 = input.lines()
+        .par_bridge()
+        .map(|str| {
+            let mut sum = 0;
+            let mut nums = str.split_whitespace()
+                .map(|s| s.trim().parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
+            let mut len = nums.len();
+            let mut zeros = false;
+            while !zeros {
+                sum += nums[len-1];
+                len -= 1;
+                zeros = true;
+                for i in 0..len {
+                    nums[i] = nums[i+1] - nums[i];
+                    zeros &= nums[i] == 0;
+                }
+            }
+            // sum ist the sum of the right triangle side
+            sum
+        })
+        .sum();
+
+    let elapsed_time = start_time.elapsed().as_secs_f64() * 1e3;
+    println!("{} ({:.3}ms)", result, elapsed_time); // 1731106378
+}
---
+++ src/bin/day_9_2.rs
@@ -0,0 +1,35 @@
+use rayon::prelude::*;
+
+fn main() {
+    let start_time = std::time::Instant::now();
+    let input = include_str!("../../data/day_9.txt");
+
+    let result: i64 = input.lines()
+        .par_bridge()
+        .map(|str| {
+            let mut nums = str.split_whitespace()
+                .map(|s| s.trim().parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
+            let mut len = 0;
+            let mut zeros = false;
+            while !zeros {
+                len += 1;
+                zeros = true;
+                for i in (len..nums.len()).rev() {
+                    nums[i] -= nums[i-1];
+                    zeros &= nums[i] == 0;
+                }
+            }
+            // 'nums[..len]' is the left triangle side
+            let r = nums[..len].iter()
+                .rev()
+                .fold(0, |acc, n| {
+                    n - acc
+                });
+            r
+        })
+        .sum();
+
+    let elapsed_time = start_time.elapsed().as_secs_f64() * 1e3;
+    println!("{} ({:.3}ms)", result, elapsed_time); // 1087
+}
